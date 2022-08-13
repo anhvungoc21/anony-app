5253b9f048bb44b813f64a7088d5f0b6ebe2d379
Anh Vu <anhvungoc.21@gmail.com>
2022-08-12 20:01:22 -0500

    Added simple navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,28 @@
-import {BiSpreadsheet} from "react-icons/bi"
+// import { BiSpreadsheet, BiWorld, BiUserCircle } from "react-icons/bi";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <div className="h-full w-1/12 bg-[color:var(--gray)] flex-col pl-4 pr-4 pb-8 pt-8 items-start">
-      <div id="logo">LOGO</div>
-      <div id="my-applications">
-      <BiSpreadsheet/>
-      WHAT
-      </div>
-      <div id="others-applications">2</div>
-      <div id="my-profile">3</div>
+    <div className="h-full w-[200px] bg-[color:var(--gray)] flex flex-col pl-4 pr-4 pb-8 pt-8 gap-2">
+      {/* <div id="logo">LOGO</div> */}
+      <Link id="my-applications" href="/dashboard/my-applications">
+        <a className="wrap-0 flex p-2 bg-white rounded-md hover:bg-[color:var(--skin)] nav--link select-none cursor-pointer gap-1">
+          <BiSpreadsheet size={28} />
+          <span> My Applications</span>
+        </a>
+      </Link>
+      <Link id="explore" href="/dashboard/explore">
+        <a className="wrap-0 flex p-2 bg-white rounded-md hover:bg-[color:var(--skin)] nav--link select-none cursor-pointer gap-1">
+          <BiWorld size={28} />
+          <span> Explore </span>
+        </a>
+      </Link>
+      <Link id="my-profile" href="/dashboard/my-profile">
+        <a className="wrap-0 flex p-2 bg-white rounded-md hover:bg-[color:var(--skin)] nav--link select-none cursor-pointer gap-1">
+          <BiUserCircle size={28} />
+          <span> My Profile </span>
+        </a>
+      </Link>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div className="bg-[color:var(--blue-color)]">
+    <div>
       HELLO
     </div>
   )
